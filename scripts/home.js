6d701c9d635a5686283da7662e479bf7eb387820
gilad chibotaru <giladchibotaru@gilads-MacBook-Pro.local>
2025-05-08 18:25:27 +0300

    blabla

---
+++ scripts/home.js
@@ -0,0 +1,36 @@
+const users = JSON.parse(localStorage.getItem("teamUp_users"));
+$('#profilePic').attr("src" , "");
+
+
+
+
+
+
+
+
+
+
+
+
+// const user = JSON.parse(localStorage.getItem('teamUp_currentUser'));
+// console.log(user);
+// $('#username').text(`${user.fullName}`);
+// $('#starter_fullname').text(`${user.fullName}`);
+// const title = document.getElementById('title');
+// title.innerHTML = `I'm 
+//                     <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer">Designer</span>
+//                     <span class="typed-cursor typed-cursor--blink" aria-hidden="true">
+//                     </span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span>
+//                     `
+// $('#image').attr("src", "../styles/assets/profilePic1.jpeg");
+// $('#aboutDesc').text(`About me :  ${user.about}`);
+// $('#usertitle').text(`Title : ${user.role}`);
+// $('#age').text(`age : ${user.age}`);
+// $('#country').text(`country : ${user.country}`);
+// for(let i in user.skills){
+//     const skills = document.getElementById(`skillsInf`);
+//     skills.innerHTML += ` <ul> 
+//                             <li>${user.skills[i]}</li> 
+//                          </ul>`;
+// }
+// $('#contactContainer').html(`<h4>Email Address:<br> ${user.email}</h4>`);
