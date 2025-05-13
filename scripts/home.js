f3dad2e64cb3de5d51848ee26a1f6ee8bbc3783f
gilad chibotaru <giladchibotaru@gilads-MacBook-Pro.local>
2025-05-13 23:57:47 +0300

    login page cleanUp

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,125 @@
 const users = JSON.parse(localStorage.getItem("teamUp_users"));
-$('#profilePic').attr("src" , "");
+const currentUser = JSON.parse(localStorage.getItem("teamUp_currentUser"));
+let urlParams = new URLSearchParams(window.location.search);
+let userId = urlParams.get('userId'); 
+let userObj;
 
+for (let i in users) {
+    let user = users[i];
+    if (+userId === user.id) {
+        console.log("Matched user:", user);
+        userObj = user;
+        break;
+    }
+}
+
+console.log("userObj after loop:", userObj);
+$('#profileName').text(`${userObj.fullName}`);
+$('#2').text(`${userObj.fullName}`);
+$('#3').attr("data-typed-items" , `${userObj.fullName}`);
+$('#1').attr("src" , userObj.picture);
+$('#aboutImage').attr("src" , userObj.picture);
+$('#aboutP').text(`${userObj.about}`);
+$('#aboutHeader').text(`${userObj.role}`);
+$('#country').text(`${userObj.country}`);
+$('#age').text(`${userObj.age}`);
+$('#email').text(`${userObj.email}`);
+for(let i in userObj.education){
+    $('#education').text(`${userObj.education[i].degree}`);
+}
+$('#phone').text(`${userObj.phone}`);
+$('#bday').text(`${userObj.dateOfBirth}`);
+$('#website').text(`${userObj.accountLinks.website}`);
+
+let sumSkills = userObj.skills.length;
+if(sumSkills % 3 == 0){
+    let divRes =  (sumSkills / 3);
+    console.log(divRes)
+    for( let i = 0; i < divRes; i++){
+        let innerSkillsContainer = document.createElement('div');
+        innerSkillsContainer.className = "col-lg-6";
+        innerSkillsContainer.id = `innerSkillsContainer[${i+1}]`;
+        $('#SkillsContainer').append(innerSkillsContainer);
+    }
+}else{
+    let divRes =  (sumSkills / 3) +1;
+    console.log(divRes)
+    for( let i = 0; i < divRes; i++){
+        let innerSkillsContainer = document.createElement('div');
+        innerSkillsContainer.className = "col-lg-6";
+        innerSkillsContainer.id = `innerSkillsContainer[${i+1}]`;
+        $('#SkillsContainer').append(innerSkillsContainer);
+    }
+    
+}
+    
+    for(let x in userObj.skills){
+        let skill = userObj.skills[x];
+        let  skillDiv = document.createElement('div');
+        skillDiv.className = 'progress';
+        if(x <= 2){
+            skillDiv.innerHTML = `<span class="skill"><span>${skill}</span> <i class="val">100%</i></span>
+            <div class="progress-bar-wrap">
+            <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>`;
+            
+            let innerSkillsContainer = document.getElementById('innerSkillsContainer[1]')
+            innerSkillsContainer.append(skillDiv);
+        }else if(x > 2 && x <= 5){
+            skillDiv.innerHTML = `<span class="skill"><span>${skill}</span> <i class="val">100%</i></span>
+            <div class="progress-bar-wrap">
+            <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>`;
+            let innerSkillsContainer = document.getElementById('innerSkillsContainer[2]')
+            innerSkillsContainer.append(skillDiv);
+            
+        }else{
+            skillDiv.innerHTML = `<span class="skill"><span>${skill}</span> <i class="val">100%</i></span>
+            <div class="progress-bar-wrap">
+            <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>`;
+            let innerSkillsContainer = document.getElementById('innerSkillsContainer[3]')
+                         innerSkillsContainer.append(skillDiv);
+         }
+    
+    
+    
+    
+    // $('#SkillsContainer').html(`<div id="innerSkillsContainer[${i}]" class="col-lg-6"</div>`);
+    //        console.log(skill)
+    //        $(`#innerSkillsContainer[${i}]`).html(`<div class="progress">
+    //                  <span class="skill"><span id="skill1">${skill}</span> <i class="val">100%</i></span>
+    //                  <div class="progress-bar-wrap">
+    //                    <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+    //                  </div>
+    //                </div>`);
+       }
+    
+console.log(sumSkills);
+
+
+
+
+
+
+
+
+
+// const users = JSON.parse(localStorage.getItem("teamUp_users"));
+// const currentUser = JSON.parse(localStorage.getItem("teamUp_currentUser"));
+// let urlParams = new URLSearchParams(window.location.search);
+// let userId = urlParams.get('userId'); 
+// let userObj
+// for(let i in users){
+//     let user = users[i];
+//     if(userId == user.id){
+//         userObj = user;
+//         break;
+//     }
+// }
+
+// console.log(userObj)
+$('#profilePic').attr("src" , "");
 
 
 
--- scripts/utils/helpers.js
+++ scripts/utils/helpers.js
@@ -16,4 +16,32 @@ export function convertImg(elementId) {
       }
     } 
   }
+
+
+   export async function fetchData(api , requestOptions){
+      let apiPath;
+    switch(api){
+      
+      case "currency":
+        apiPath = `https://api.currencyfreaks.com/v2.0/iptocurrency?from=gbp&amount=500&ip=182.186.18.9&apikey=2e639b05f01540cdb5bd7a5f7d488af7", ${requestOptions}`
+        break;
+      case "weatherForecast":
+        apiPath = `http://api.weatherapi.com/v1/forecast.json?key=08e22e247bb14da1943191555251305&${requestOptions}&aqi=no&alerts=no`
+        break;
+      case "weatherCurrent":
+        apiPath = `http://api.weatherapi.com/v1/current.json?key=08e22e247bb14da1943191555251305&${requestOptions}&aqi=no&alerts=no`
+        break;
+      let fetchResponse = await fetch(apiPath, {
+          method: "GET",
+          headers: {
+              "Content-Type": "application/json",
+          
+          }
+      })
+      let response = await fetchResponse.json();
+      return response
+  }
+
+  }
+  
   
